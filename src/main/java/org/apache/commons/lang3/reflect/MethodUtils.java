20ed0f244acb4ab948e029f8489e5d093506d82d
SeasonPan <244014926@qq.com>
2023-02-23 20:11:32 +0800

    [LANG-1694]MethodUtils.getMatchingMethod

--- src/main/java/org/apache/commons/lang3/reflect/MethodUtils.java
+++ src/main/java/org/apache/commons/lang3/reflect/MethodUtils.java
@@ -751,8 +751,8 @@ public class MethodUtils {
         }
 
         final List<Method> bestCandidates = candidates.values().iterator().next();
-        if (bestCandidates.size() == 1 || (bestCandidates.size() > 1 && !Objects.equals(bestCandidates.get(0).getDeclaringClass(),
-                bestCandidates.get(1).getDeclaringClass()))) {
+        if (bestCandidates.size() == 1 || !Objects.equals(bestCandidates.get(0).getDeclaringClass(),
+                bestCandidates.get(1).getDeclaringClass())) {
             return bestCandidates.get(0);
         }
 
--- src/test/java/org/apache/commons/lang3/reflect/MethodUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/reflect/MethodUtilsTest.java
@@ -1054,6 +1054,8 @@ public class MethodUtilsTest extends AbstractLangTest {
         assertThrows(IllegalStateException.class,
                 () -> MethodUtils.getMatchingMethod(GetMatchingMethodClass.class, "testMethod4", null, null));
 
+        assertEquals(MethodUtils.getMatchingMethod(GetMatchingMethodImpl.class, "testMethod5", RuntimeException.class),
+                GetMatchingMethodImpl.class.getMethod("testMethod5", Exception.class));
     }
 
     private static final class GetMatchingMethodClass {
@@ -1091,4 +1093,14 @@ public class MethodUtilsTest extends AbstractLangTest {
         }
     }
 
+
+    protected abstract static class AbstractGetMatchingMethod {
+        public abstract void testMethod5(Exception exception);
+    }
+
+    private static class GetMatchingMethodImpl extends AbstractGetMatchingMethod {
+        @Override
+        public void testMethod5(final Exception exception) {
+        }
+    }
 }
